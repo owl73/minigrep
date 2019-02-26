faa57e35a45aa5e20b157cc555e4410931f9a272
owl73 <olowry73@gmail.com>
2019-02-26 09:10:50 -0500

    Revert "Add command line arguments"
    This reverts commit 952ea6b2808bdd0651d78de2154dc67440e7e0d6.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use std::env;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Hello, world!");
 }
